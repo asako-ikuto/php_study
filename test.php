97a56ea0ea9ce2e904df78524ce56753f3060b08
ikuto-asako <m131932@gmail.com>
2022-03-20 12:12:40 +0900

    php課題1-1作成

--- test.php
+++ test.php
@@ -7,6 +7,24 @@
 </head>
 
 <body>
+    <h2>日本の首都は？</h2>
+    <form action="" method="post">
+        <input type="text" name="capital_answer">
+        <input type="submit" value="OK">
+        <!-- 正解・不正解判定結果 -->
+        <p style="font-size:1.2rem">
+            <?php
+            if (isset($_POST['capital_answer'])) {
+                $capital_answer = htmlspecialchars($_POST['capital_answer']);
+                if ($capital_answer === '東京') {
+                    echo '正解';
+                } else {
+                    echo '不正解';
+                }
+            }
+            ?>
+        </p>
+    </form>
 </body>
 
 </html>
